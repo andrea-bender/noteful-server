8c158d473eceb7d4f7bd50b9f0a39b5c7ac5ade2
Andrea Bender <andreab.1726@gmail.com>
2020-02-05 16:12:11 -0700

    updates

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const foldersRouter = require('./folders/folders-router');
 const notesRouter = require('./notes/notes-router');
@@ -14,25 +16,23 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 
-// const allowedOrigins = [ 'https://client.andreabender.now.sh' ];
+const allowedOrigins = [ 'https://client.andreabender.now.sh' ];
+app.use(
+	cors({
+		origin: function(origin, callback) {
+			// allow requests with no origin - like mobile apps, curl, postman
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		}
+	})
+);
+app.use(helmet());
 
-app.use('/api/folders', foldersRouter);
-app.use('/api/notes', notesRouter);
-
-app.get('/', (req, res) => {
-	res.json({ message: 'Hello, world!' });
-});
-
-//error handler middleware
-// app.use(function errorHandler(error, req, res, next) {
-// 	let response;
-// 	console.error(error);
-// 	if (NODE_ENV === 'production') {
-// 		response = { error: { message: 'server error' } };
-// 	} else {
-// 		response = { error: { message: 'server error' } };
-// 	}
-// 	res.status(500).json(response);
-// });
+app.use('/folders', foldersRouter);
+app.use('/notes', notesRouter);
 
 module.exports = app;
