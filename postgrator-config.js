25051d79f85e58405cd2f4da1e14c388d49b7868
Andrea Bender <andreab.1726@gmail.com>
2020-02-05 15:45:56 -0700

    updates

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,9 @@
+/* eslint-disable strict */
 require('dotenv').config();
 
 module.exports = {
-    'migrationsDirectory': 'migrations',
-    'driver': 'pg',
-    'connectionString': (process.env.NODE_ENV === 'test')
-        ? process.env.TEST_DATABASE_URL
-        : process.env.DATABASE_URL,
-    ssl: !!process.env.SSL,
+	migrationsDirectory: 'migrations',
+	driver: 'pg',
+	connectionString: process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+	ssl: !!process.env.SSL
 };
--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+/* eslint-disable strict */
 /* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
@@ -10,31 +12,43 @@ const notesRouter = require('./notes/notes-router');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
 
-app.use('/api/folders',foldersRouter);
+const allowedOrigins = [ 'https://client.andreabender.now.sh' ];
+app.use(
+	cors({
+		origin: function(origin, callback) {
+			// allow requests with no origin - like mobile apps, curl, postman
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		}
+	})
+);
+app.use(helmet());
+
+app.use('/api/folders', foldersRouter);
 app.use('/api/notes', notesRouter);
 
 app.get('/', (req, res) => {
-    res.json({message: 'Hello, world!'});
+	res.json({ message: 'Hello, world!' });
 });
 
 //error handler middleware
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  console.error(error);
-  if(NODE_ENV === 'production'){
-    response = { error : { message: 'server error'} };
-  } else {
-    response = { error : { message: 'server error'} };
-  }
-  res.status(500).json(response);
-});
+// app.use(function errorHandler(error, req, res, next) {
+// 	let response;
+// 	console.error(error);
+// 	if (NODE_ENV === 'production') {
+// 		response = { error: { message: 'server error' } };
+// 	} else {
+// 		response = { error: { message: 'server error' } };
+// 	}
+// 	res.status(500).json(response);
+// });
 
 module.exports = app;
