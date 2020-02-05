435e66ac9416b269abd6d4ca36a4ab40b9b12bce
Andrea Bender <andreab.1726@gmail.com>
2020-02-05 16:06:18 -0700

    updates

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const foldersRouter = require('./folders/folders-router');
 const notesRouter = require('./notes/notes-router');
@@ -17,8 +15,6 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 
 // const allowedOrigins = [ 'https://client.andreabender.now.sh' ];
-app.use(cors());
-app.use(helmet());
 
 app.use('/api/folders', foldersRouter);
 app.use('/api/notes', notesRouter);
