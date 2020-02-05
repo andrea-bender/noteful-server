232b7425fe5ca15ea46892ad046c2e118ff58e90
Andrea Bender <andreab.1726@gmail.com>
2020-02-05 15:56:18 -0700

    updates

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
+/* eslint-disable strict */
 /* eslint-disable no-console */
 const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
-  client: 'pg',
-  connection: DATABASE_URL
+	client: 'pg',
+	connection: DATABASE_URL
 });
 
 //use to set the Knex instance
 app.set('db', db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+	console.log(`Server listening at http://localhost:${PORT}`);
 });
-
 
 //Note: Here where Knex instance is created
 //The server.js file attaches the Knex instance to the app as a property called 'db'
