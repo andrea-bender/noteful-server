fb2f49c8998def0676800bb192c13a4a5e34ee0b
Andrea Bender <andreab.1726@gmail.com>
2020-02-05 15:58:44 -0700

    updates

--- src/app.js
+++ src/app.js
@@ -16,20 +16,8 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 
-const allowedOrigins = [ 'https://client.andreabender.now.sh' ];
-app.use(
-	cors({
-		origin: function(origin, callback) {
-			// allow requests with no origin - like mobile apps, curl, postman
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-				return callback(new Error(msg), false);
-			}
-			return callback(null, true);
-		}
-	})
-);
+// const allowedOrigins = [ 'https://client.andreabender.now.sh' ];
+app.use(cors());
 app.use(helmet());
 
 app.use('/api/folders', foldersRouter);
